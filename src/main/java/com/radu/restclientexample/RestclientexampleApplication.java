124708e6a10e46c53f293577bc127568d2ed3047
Radu Radu <neoradu@gmail.com>
2022-09-08 11:19:54 +0300

    used https://start.spring.io/  to make new project
    Signed-off-by: Radu Radu <neoradu@gmail.com>

--- src/main/java/com/radu/restclientexample/RestclientexampleApplication.java
+++ src/main/java/com/radu/restclientexample/RestclientexampleApplication.java
@@ -1,12 +1,13 @@
-package guru.springframework.springrestclientexamples;
+package com.radu.restclientexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringRestClientExamplesApplication {
+public class RestclientexampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringRestClientExamplesApplication.class, args);
+		SpringApplication.run(RestclientexampleApplication.class, args);
 	}
+
 }
---
+++ src/test/java/com/radu/restclientexample/RestclientexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radu.restclientexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestclientexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
